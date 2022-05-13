6d66adacd25cd398e49c4352c1c3e571617bec09
Oshadi Dilanka <oshidilanka99@outlook.com>
2022-05-13 08:59:05 +0530

    commit2

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.get("/stocks", async (req,res) => {
         user: process.env.DB_USER,
         password: process.env.DB_PASS,
         database: process.env.DB_NAME,
-        socketPath: '/cloudsql/${process.env.INSTANCE_CONNECTION_NAME',
+        socketPath: '/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}',
 
 })
 
